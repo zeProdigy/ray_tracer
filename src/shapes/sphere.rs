cb2e76d77fd88013b6da70ce30389ba7edc8866f
zeProdigy <zeprodigy@outlook.com>
2019-03-11 22:37:44 +0300

    Fix bug in sphere intersection & minimal refactoring

--- src/shapes/sphere.rs
+++ src/shapes/sphere.rs
@@ -14,11 +14,11 @@ impl Intersection for Sphere {
     fn is_intersect(&self, ray: &core::Ray, tmin: f32, tmax: f32) -> (bool, f32) {
         let c = &self.center;
         let r = &self.radius;
-        let vect = core::Point(ray.origin.0 - c.0, ray.origin.1 - c.1, ray.origin.2 - c.2);
+        let oc = math::subtract(ray.origin, &c);
 
         let k1 = math::dot(ray.direction, ray.direction);
-        let k2 = 2.0 * math::dot(&vect, ray.direction);
-        let k3 = math::dot(&vect, &vect) - r * r;
+        let k2 = 2.0 * math::dot(&oc, ray.direction);
+        let k3 = math::dot(&oc, &oc) - r * r;
 
         let d: f32 = k2 * k2 - 4.0 * k1 * k3;
 
@@ -26,15 +26,15 @@ impl Intersection for Sphere {
             return (false, 0.0);
         }
 
-        let c1: f32 = (-k2 + d.sqrt()) / (2.0 * k1);
-        let c2: f32 = (-k2 - d.sqrt()) / (2.0 * k1);
+        let t1: f32 = (-k2 + d.sqrt()) / (2.0 * k1);
+        let t2: f32 = (-k2 - d.sqrt()) / (2.0 * k1);
 
-        if (c1 > tmin) && (c1 < tmax) {
-            return (true, c1);
+        if (t2 > tmin) && (t2 < tmax) {
+            return (true, t2);
         }
 
-        if (c2 > tmin) && (c2 < tmax) {
-            return (true, c2);
+        if (t1 > tmin) && (t1 < tmax) {
+            return (true, t1);
         }
 
         (false, 0.0)
